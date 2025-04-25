ad816de4745eee3413259f825b3ac8d62f3a121c
Adrian Valentin Kragh-Hillers <adrianvalentin@live.dk>
2025-04-25 13:48:31 +0200

    Update to generate config and jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-#BSUB -J jojojob_videogen              
-#BSUB -q gpuv100                  
+#BSUB -J vidgen1           
+#BSUB -q gpuv100           
 #BSUB -W 01:30                     # Wall time: 30 minutes
-#BSUB -R "rusage[mem=10000MB]"    
+#BSUB -R "rusage[mem=10GB]"    
 #BSUB -o cool%J.out            
 #BSUB -e cool%J.err            
-#BSUB -u bbjar2303@gmail.com      
-#BSUB -B                           # Send email at start
-#BSUB -N                           # Send email at end
-#BSUB -n 10 
+
+#BSUB -n 1
 #BSUB -R "span[hosts=1]"          
 
 # Load CUDA module (update if needed)
 module load cuda/11.7
 
 # Activate the local virtual environment
-source /work3/s204104/ADLCV/VideoGeneration/vidgen_venv/bin/activate
+#source /work3/s201390/ADLCV/VideoGeneration/
+source motion_guidance_env/bin/activate
 
 # Optional: set PYTHONPATH if custom modules are needed
-export PYTHONPATH=/work3/s204104/ADLCV/VideoGeneration:$PYTHONPATH
+#export PYTHONPATH=/work3/s204104/ADLCV/VideoGeneration:$PYTHONPATH
 
 ## For memory
 export HOME=$PWD
@@ -39,14 +38,9 @@ mkdir -p "$HOME/.cache" "$HOME/.config" "$HOME/.huggingface"
 #    --num_recursive_steps 1
 
 
+#export PYTHONPATH=$PYTHONPATH:/work3/s201390/VideoGeneration/motion_guidance_env/src/taming-transformers
+export PYTHONPATH=/VideoGeneration/motion_guidance_env/src/taming-transformers
 
+export PYTHONPATH=/work3/s201390/VideoGeneration/motion_guidance_env/src/taming-transformers
 # Run your script
 python ./generate.py \
-    --prompt "" \
-    --input_dir ./data/teapot \
-    --edit_mask_path down150.mask.pth \
-    --target_flow_name down150.pth \
-    --use_cached_latents \
-    --save_dir results/teapot.right.rec_step_1_no_text_prompt \
-    --log_freq 5 \
-    --num_recursive_steps 1
